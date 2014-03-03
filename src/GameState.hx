b4f3119bc52e63fe6b04fe6d78fea3beaaea7ddc
Marco Kaul <Marco.Kaul@ueberbit.de>
2014-03-03 19:11:44 +0100

    Add camera, fix map

--- src/GameState.hx
+++ src/GameState.hx
@@ -1,28 +1,29 @@
 package;
 
 import entity.Link;
+import flixel.FlxCamera;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import map.OverworldMap;
 
 class GameState extends FlxState {
-	var player:Link;
-	var map:OverworldMap;
+    var player:Link;
+    var map:OverworldMap;
 
     override public function create():Void {
-        trace("state created");
-
-       	map = new OverworldMap();
+        map = new OverworldMap();
         this.add(map);
         
         player = new Link();
         this.add(player);
+
+        FlxG.camera.follow(player, FlxCamera.STYLE_SCREEN_BY_SCREEN);
     }
 
     override public function update() {
-    	super.update();
-    	
-    	FlxG.collide(player, map);
+        super.update();
+
+        FlxG.collide(player, map);
     }
 }
